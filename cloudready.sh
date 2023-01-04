b5615fb137b6a03211a94c06bae43683a8b70e0d
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2023-01-04 15:57:16 +0000

    fix globbing issue
    may have bugs

--- cloudready.sh
+++ cloudready.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tar -caf $1 *.*
-gpg --no-symkey-cache --cipher-algo AES256 --symmetric $1
-rm $1
+tar -caf "$1" *.*
+gpg --no-symkey-cache --cipher-algo AES256 --symmetric "$1"
+rm "$1"
